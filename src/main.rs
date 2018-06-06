80d0e53ef84006ba48edf64b4318129ce57a506d
Geoff Maggi <gmaggi@pdx.edu>
2018-06-05 18:28:27 -0700

    Added paddle and inputs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate rand;
 
 use ggez::conf;
 use ggez::event;
+use ggez::event::{Keycode, Mod};
 use ggez::graphics::{self, set_color, DrawMode, Point2};
 use ggez::{Context, ContextBuilder, GameResult};
 use std::{env, path};
@@ -13,6 +14,8 @@ use rand::Rng;
 
 const WINDOW_W: u32 = 400;
 const WINDOW_H: u32 = 600;
+const PADDLE_W: f32 = 80.0;
+const PADDLE_H: f32 = 10.0;
 
 struct Ball {
     x: f32,
@@ -27,14 +30,14 @@ impl Ball {
         let mut rng = rand::thread_rng();
 
         let mut vel_x = rng.gen::<f32>(); //This will change based on paddle velocity
-        let vel_y = 1.0; //Starting Speed
+        let vel_y = 5.0; //Starting Speed
 
         Ball {
             x: WINDOW_W as f32 / 2.0,
             y: WINDOW_H as f32 / 2.0,
             vel_x: vel_x,
             vel_y: vel_y,
-            radius: 10.0,
+            radius: 8.0,
         }
     }
 
@@ -53,14 +56,72 @@ impl Ball {
     }
 }
 
+struct Paddle {
+    x: f32,
+    vel_x: f32,
+    moving: bool,
+}
+
+impl Paddle {
+    fn new(_ctx: &mut Context) -> Paddle {
+        Paddle {
+            x: 300.0,
+            vel_x: 0.0,
+            moving: false,
+        }
+    }
+
+    pub fn update(&mut self) {
+        if self.moving {
+            self.x += self.vel_x;
+        }
+        if self.x <= 0.0 {
+            self.x = 0.0;
+        }
+        if self.x + PADDLE_W >= WINDOW_W as f32 {
+            self.x = WINDOW_W as f32 - PADDLE_W;
+        }
+    }
+
+    pub fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        set_color(ctx, [1.0, 1.0, 1.0, 1.0].into())?;
+
+        let rect = graphics::Rect::new(
+            self.x,
+            WINDOW_H as f32 - 10.0 - PADDLE_H,
+            PADDLE_W,
+            PADDLE_H,
+        );
+        graphics::rectangle(ctx, DrawMode::Fill, rect)?;
+        Ok(())
+    }
+
+    pub fn move_left(&mut self) {
+        self.vel_x = -10.0;
+        self.moving = true;
+    }
+
+    pub fn move_right(&mut self) {
+        self.vel_x = 10.0;
+        self.moving = true;
+    }
+
+    pub fn stop(&mut self) {
+        self.vel_x = 0.0;
+        self.moving = false;
+    }
+}
+
 struct MainState {
     ball: Ball,
+    paddle: Paddle,
 }
 
 impl MainState {
     fn new(_ctx: &mut Context) -> GameResult<MainState> {
         let s = MainState {
             ball: Ball::new(_ctx),
+            paddle: Paddle::new(_ctx),
         };
         Ok(s)
     }
@@ -87,18 +148,48 @@ impl MainState {
 
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        self.paddle.update();
         self.ball.update();
         self.collision();
         Ok(())
     }
 
-    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx);
+    fn draw(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(_ctx);
 
-        self.ball.draw(ctx)?;
+        self.paddle.draw(_ctx)?;
+        self.ball.draw(_ctx)?;
 
-        graphics::present(ctx);
+        graphics::present(_ctx);
         Ok(())
+    }
+
+    fn key_down_event(
+        &mut self,
+        _ctx: &mut Context,
+        keycode: Keycode,
+        _keymod: Mod,
+        _repeat: bool,
+    ) {
+        match keycode {
+            Keycode::Left | Keycode::A => {
+                self.paddle.move_left();
+            }
+            Keycode::Right | Keycode::D => {
+                self.paddle.move_right();
+            }
+            Keycode::Escape => _ctx.quit().unwrap(),
+            _ => {}
+        }
+    }
+
+    fn key_up_event(&mut self, _ctx: &mut Context, keycode: Keycode, _keymod: Mod, _repeat: bool) {
+        match keycode {
+            Keycode::Left | Keycode::A | Keycode::Right | Keycode::D => {
+                self.paddle.stop();
+            }
+            _ => {}
+        }
     }
 }
 
